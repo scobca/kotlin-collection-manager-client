641c77fecaeeac1bbe93b2aca7f58d18b7430b58
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-23 01:55:41 +0300

    build(deps): updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.serialization") version "2.1.20"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("it.nicolasfarabegoli.conventional-commits") version "3.1.3"
@@ -27,8 +28,11 @@ repositories {
 
 dependencies {
     //Spring
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.integration:spring-integration-ip")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-integration")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
