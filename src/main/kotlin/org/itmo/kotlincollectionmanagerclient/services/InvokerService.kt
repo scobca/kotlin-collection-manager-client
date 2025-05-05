16c00ee0a30f3e360f2d88508e2efc94596649df
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-05 21:01:09 +0300

    feat(api): moved commands history to client application

--- src/main/kotlin/org/itmo/kotlincollectionmanagerclient/services/InvokerService.kt
+++ src/main/kotlin/org/itmo/kotlincollectionmanagerclient/services/InvokerService.kt
@@ -1,11 +1,12 @@
 package org.itmo.kotlincollectionmanagerclient.services
 
+import org.itmo.kotlincollectionmanagerclient.storages.CommandsHistory
 import org.itmo.kotlincollectionmanagerclient.utils.TcpConnectionFactory
 import org.springframework.stereotype.Service
 import java.util.Scanner
 
 @Service
-class InvokerService {
+class InvokerService(private val commandsHistory: CommandsHistory) {
     private var runtime = true
 
     fun run() {
@@ -14,13 +15,27 @@ class InvokerService {
         print("> ")
         while (runtime) {
             val line = scanner.nextLine().trim()
+            val command = line.trim().split(" ")[0]
 
             if (line == "") {
                 print("> ")
                 continue
             }
 
+            if (command == "history") {
+                val response = commandsHistory.getCommandsHistory()
+                commandsHistory.addCommand(command)
+
+                println(response)
+                print("> ")
+                continue
+            }
+
             val response = TcpConnectionFactory.sendMessage(line)
+
+            if (!response.contains("Command $command not found")) {
+                commandsHistory.addCommand(command)
+            }
 
             println(response)
             print("> ")
