a5dca9981819f368f5c7c3eeb85f9fd6da3ce84e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-07 02:48:18 +0300

    refactor(invoker): moved invoker logic to external class

--- src/main/kotlin/org/itmo/kotlincollectionmanagerclient/services/InvokerService.kt
+++ src/main/kotlin/org/itmo/kotlincollectionmanagerclient/services/InvokerService.kt
@@ -1,23 +1,14 @@
 package org.itmo.kotlincollectionmanagerclient.services
 
-import org.itmo.kotlincollectionmanagerclient.commands.HistoryCommand
-import org.itmo.kotlincollectionmanagerclient.storages.CommandsHistory
-import org.itmo.kotlincollectionmanagerclient.utils.CommandValidatorDistributor
-import org.itmo.kotlincollectionmanagerclient.utils.ServerWatcherUtil
-import org.itmo.kotlincollectionmanagerclient.utils.StringToMapParser
-import org.itmo.kotlincollectionmanagerclient.utils.TcpConnectionFactory
+import org.itmo.kotlincollectionmanagerclient.utils.InvokerLogic
 import org.springframework.stereotype.Service
 import java.util.Scanner
 
 @Service
 class InvokerService(
-    private val distributor: CommandValidatorDistributor,
-    private val serverWatcher: ServerWatcherUtil,
-    private val historyCommand: HistoryCommand,
-    private val commandsHistory: CommandsHistory,
+    private val invokerLogic: InvokerLogic,
 ) {
     private var runtime = true
-    private val validatableCommands = listOf<String>("insert", "update", "replaceIfLower")
 
     fun run() {
         val scanner = Scanner(System.`in`)
@@ -25,52 +16,10 @@ class InvokerService(
         print("> ")
         while (runtime) {
             val line = scanner.nextLine().trim()
-            val parts = line.trim().split(" ")
-            val command = parts.firstOrNull() ?: ""
-            val args = parts.drop(1)
+            val response = invokerLogic.handleCommand(line)
 
-            if (line == "") {
-                print("> ")
-                continue
-            }
-
-            if (command == "exit") println("Application stopped."); runtime = false; continue
-
-            if (validatableCommands.contains(command)) {
-                if (serverWatcher.checkConnection()) {
-                    println(distributor.distribute(command, args))
-                    print("> ")
-                    continue
-                } else {
-                    continue
-                }
-            }
-
-            if (command == "history") {
-                historyCommand.run()
-                continue
-            }
-
-            val response = TcpConnectionFactory.sendMessage(line)
-
-            if (!response.contains("Command $command not found")) {
-                commandsHistory.addCommand(command)
-            }
-
-            if (command == "help") {
-                val correctResponse = StringToMapParser.parseToMap(response)
-                val bold = "\u001B[1m"
-                val reset = "\u001B[0m"
-
-                correctResponse.forEach { (key, value) ->
-                    println("${bold}$key ${reset}: $value")
-                }
-                print("> ")
-                continue
-            }
-
-            println(response)
-            print("> ")
+            if (response == "STOP") runtime = false
+            if (response == "") continue
         }
     }
 }
---
+++ src/main/kotlin/org/itmo/kotlincollectionmanagerclient/utils/InvokerLogic.kt
@@ -0,0 +1,72 @@
+package org.itmo.kotlincollectionmanagerclient.utils
+
+import org.itmo.kotlincollectionmanagerclient.commands.HistoryCommand
+import org.itmo.kotlincollectionmanagerclient.storages.CommandsHistory
+import org.springframework.stereotype.Component
+
+@Component
+class InvokerLogic(
+    private val serverWatcher: ServerWatcherUtil,
+    private val distributor: CommandValidatorDistributor,
+    private val historyCommand: HistoryCommand,
+    private val commandsHistory: CommandsHistory
+) {
+    private val validatableCommands = listOf<String>("insert", "update", "replaceIfLower")
+
+    fun handleCommand(line: String): String {
+        val parts = line.trim().split(" ")
+        val command = parts.firstOrNull() ?: ""
+        val args = parts.drop(1)
+
+        if (line.trim() == "") {
+            print("> ")
+            return ""
+        }
+
+        if (command == "exit") {
+            println("Application stopped.")
+            return "STOP"
+        }
+
+        if (validatableCommands.contains(command)) {
+            if (serverWatcher.checkConnection()) {
+                println(distributor.distribute(command, args))
+                print("> ")
+                return ""
+            } else {
+                return ""
+            }
+        }
+
+        if (command == "history") {
+            historyCommand.run()
+            return ""
+        }
+
+        val response = TcpConnectionFactory.sendMessage(line)
+
+        if (!response.contains("Command $command not found")) {
+            commandsHistory.addCommand(command)
+        }
+
+        if (command == "help") {
+            val correctResponse = StringToMapParser.parseToMap(response)
+            val bold = "\u001B[1m"
+            val reset = "\u001B[0m"
+
+            if (!correctResponse.isEmpty()) {
+                correctResponse.forEach { (key, value) ->
+                    println("${bold}$key ${reset}: $value")
+                }
+            } else {
+                println("Commands not loaded")
+            }
+            print("> ")
+            return ""
+        }
+
+        println(response)
+        print("> ")
+        return ""
+    }
+}
