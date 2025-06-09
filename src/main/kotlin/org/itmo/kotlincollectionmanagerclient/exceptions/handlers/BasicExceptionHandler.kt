2c10b67ffbc56b14ea2b4b8ddfac8dac1f33c966
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-10 01:18:40 +0300

    feat(api): created basic exception handler

---
+++ src/main/kotlin/org/itmo/kotlincollectionmanagerclient/exceptions/handlers/BasicExceptionHandler.kt
@@ -0,0 +1,33 @@
+package org.itmo.kotlincollectionmanagerclient.exceptions.handlers
+
+import org.itmo.kotlincollectionmanagerclient.exceptions.AuthenticationException
+import org.itmo.kotlincollectionmanagerclient.exceptions.ServerNotAvailableException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class BasicExceptionHandler {
+    @ExceptionHandler
+    fun handlerUnauthorizedException(e: ServerNotAvailableException): ResponseEntity<ErrorMessage?> {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+            ErrorMessage(
+                statusCode = HttpStatus.SERVICE_UNAVAILABLE.value(),
+                errorName = e.javaClass.simpleName,
+                message = e.message ?: "Message not provided"
+            )
+        )
+    }
+
+    @ExceptionHandler
+    fun handlerUnauthorizedException(e: AuthenticationException): ResponseEntity<ErrorMessage?> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+            ErrorMessage(
+                statusCode = HttpStatus.UNAUTHORIZED.value(),
+                errorName = e.javaClass.simpleName,
+                message = e.message ?: "Message not provided"
+            )
+        )
+    }
+}
